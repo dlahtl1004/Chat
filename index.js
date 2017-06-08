a73b563169dd568a98eae74afc377fff3cee11a2
dlahtl1004 <dlqjagur2@naver.com>
2017-06-08 13:29:56 +0900

    modify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var PORT = process.env.PORT || 8000;
 var fs = require('fs');
+var http = require('http');
 var app =http.createServer(function(req, res){
     fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
