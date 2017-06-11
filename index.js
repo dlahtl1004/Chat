661a39c5bb9622fb97ffb1986febc61b9ae85b37
dlahtl1004 <dlqjagur2@naver.com>
2017-06-11 22:56:46 +0900

    Quiz

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ io.sockets.on('connection', socket => {
     socket.emit('connection', {
         type : 'connected'
     });
-
+                
     socket.on('connection', data => {
 
         if(data.type === 'join') {
@@ -59,7 +59,18 @@ io.sockets.on('connection', socket => {
         // });
         
         var room = socket.room;
-        
+        if(data.message == Quiz){
+            if(room){
+                socket.emit('system', {
+                    message : `${data.message} is correct answer`
+                });
+                socket.broadcast.to(room).emit('eraseQuiz');
+                socket.broadcast.to(room).emit('system', {
+                    message : `${data.message} is correct answer`
+                });
+                Quiz = '';
+            }
+        }
         if(room) {
             socket.broadcast.to(room).emit('message', data);
         }
@@ -91,6 +102,34 @@ io.sockets.on('connection', socket => {
             socket.broadcast.to(room).emit('erase');
         }
     });
+    
+    socket.on('disconnect',function(data){
+        var room = socket.room;
+        if(isHost == 1){
+            isHost = 0;
+            if(room){
+                socket.broadcast.to(room).emit('setHost',{
+                    isHost : 0
+                });
+            }
+            
+        }
+    });
+    
+    socket.on('setHost',data =>{
+        if(isHost == 0){
+            socket.emit('setHost',{
+                isHost : 1
+            });
+            isHost = 1;
+            socket.emit('system', {
+                message : 'your host'
+            });
+        }
+    });
+    
+    socket.on('addQuiz', function(data){
+        Quiz = data.quiz;
+    })
 });
-
 
